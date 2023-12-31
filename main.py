bdd91ad2d575205669ebfcfaff9738954d1f9bc2
Preetham Pemmasani <ppmpreetham1@gmail.com>
2023-12-31 22:04:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Only Whole Numbers are allowed in the list
-# O(n^2) time complexity
+# O(n) time complexity
 
 def counter_sort(lst:list) -> list:
     counter=0
     sorted_list=[]
-    while True:
+    while len(sorted_list)<=len(lst):
         counter+=1
         if counter in lst:
             sorted_list.append(counter)
-        if len(sorted_list)==len(lst):
-            return sorted_list
+        else:
+            break
+    return sorted_list
+
+print(counter_sort([1,4,2,3,5,6,8,7,9,10]))    
