86fd6c26c030447f3cc034d5be05befddd595781
Preetham Pemmasani <75422607+Preetham-ai@users.noreply.github.com>
2024-01-03 13:49:17 +0530

    Create counter-sort.rs

---
+++ counter-sort.rs
@@ -0,0 +1,15 @@
+fn counter_sort(lst: Vec<usize>) -> Vec<usize> {
+    let mut counter = 0;
+    let mut sorted_list = Vec::new();
+
+    while sorted_list.len() <= lst.len() {
+        counter += 1;
+        if lst.contains(&counter) {
+            sorted_list.push(counter);
+        } else {
+            break;
+        }
+    }
+
+    sorted_list
+}
